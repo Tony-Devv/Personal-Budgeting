b2f7455ea5c0a1ad9c8beeaaa8c20456d5037882
Engshadow <161144573+Engshadow@users.noreply.github.com>
2025-05-07 21:18:07 +0300

    added controllers

--- Controller/BudgetController.cs
+++ Controller/BudgetController.cs
@@ -1,6 +1,32 @@
 using Model.Entities;
+using Model.Handlers;
+using Model.Utilities;
 
-public class ReminderController
+public class BudgetController
 {
-	public bool TrySendReminders(List<Expense> expenses) { return true; }
+    private readonly BudgetHandler _budgetHandler = ServicesContainer.Instance.GetService<BudgetHandler>();
+
+    public async Task<(bool Success, int BudgetId)> TryAddBudget(Budget budget)
+    {
+        int result = await _budgetHandler.AddNewBudget(budget);
+        return (result != -1, result);
+    }
+
+    public async Task<(bool Success, int BudgetId)> TryUpdateBudget(Budget newValues)
+    {
+        var updatedBudget = await _budgetHandler.UpdateBudget(newValues);
+        return (updatedBudget != null, updatedBudget);
+    }
+
+    public async Task<bool> TryDeleteBudget(Budget budget)
+    {
+        int result = await _budgetHandler.DeleteBudget(budget);
+        return result != -1;
+    }
+
+    public async Task<bool> TryGetBudgetByName(int userId, string budgetName)
+    {
+        var budget = await _budgetHandler.GetBudgetByName(userId, budgetName);
+        return budget != null;
+    }
 }
--- Controller/ExpenseController.cs
+++ Controller/ExpenseController.cs
@@ -1,10 +1,41 @@
 using Model.Entities;
+using Model.Handlers;
+using Model.Utilities;
 
 public class ExpenseController
 {
-	public bool TryAddNewExpense(Expense expense) { return true; }
-	public bool TryDeleteExpense(Expense expense) { return true; }
-	public bool TrySearchExpense(string expense, int userId) { return true; }
-	public bool TryGetExpensesWithReminders(int userId) { return true; }
-	public bool TrySetExpenseWithTimeReminder(Expense expense, DateTime time) { return true; }
+    private readonly ExpenseHandler _expenseHandler = ServicesContainer.Instance.GetService<ExpenseHandler>();
+
+    public async Task<(bool Success, int ExpenseId)> TryAddExpense(Expense expense)
+    {
+        int result = await _expenseHandler.AddNewExpense(expense);
+        return (result != -1, result);
+    }
+
+    public async Task<(bool Success, int ExpenseId)> TryUpdateExpense(int expenseId, Expense newValues)
+    {
+        var updatedExpense = await _expenseHandler.UpdateExpense(expenseId, newValues);
+        return (updatedExpense != null, updatedExpense);
+    }
+
+    public async Task<bool> TryDeleteExpense(Expense expense)
+    {
+        int result = await _expenseHandler.DeleteExpense(expense);
+        return result != -1;
+    }
+
+    public async Task<bool> TrySearchExpense(string expenseName, int userId)
+    {
+        var expense = await _expenseHandler.SearchExpenseByName(userId, expenseName);
+        return expense != null;
+    }
+
+
+    public async Task<bool> TrySetExpenseReminder(int expenseId, DateTime reminderTime)
+    {
+        var Reminder = await _expenseHandler.GetExpensesThatHasReminders(expenseId);
+        return Reminder!=null;   
+    }
 }
+
+
