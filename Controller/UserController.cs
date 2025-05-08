77a185dbf1afc18e5ee7223604f0655d6ff13cfb
Mohamed Adel Kamal <raynowing007@gmail.com>
2025-05-08 20:38:36 +0300

    User Controller Input Validator using FluentValidation, will add validations for the rest of the controllers next

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,49 +1,151 @@
+using System.ComponentModel.DataAnnotations;
+using Controller.Validators;
+using FluentValidation;
+using FluentValidation.Internal;
 using Model.Entities;
 using Model.Handlers;
 using Model.Utilities;
 
 public class UserController
 {
+    public enum UserValidationsRules
+    {
+        Register,
+        Login,
+        Edit,
+        ChangePassword,
+        GetUserData
+    }
+    
     private readonly UserHandler _userHandler = ServicesContainer.Instance.GetService<UserHandler>();
+    private readonly UserInputValidator _userInputValidator = new UserInputValidator();
 
-    public async Task<(bool Success, int UserId)> TryAddUser(User user)
+    
+    public async Task<(bool Success, int UserId,List<string> errorMessages)> TryAddUser(User user) // valid (UserName,Email,Password,PhoneNumber)
     {
+        var context = CreateContext(user, UserValidationsRules.Register);
+        
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, -1, errors);
+        }
+        
         int result = await _userHandler.RegisterNewUser(user);
-        return (result != -1, result);
+        return (result != -1, result,new List<string>());
     }
 
-    public async Task<(bool Success, User? UpdatedUser)> TryUpdateUser(User newValues)
+    public async Task<(bool Success, User? UpdatedUser,List<string> errors)> TryUpdateUser(User newValues)
     {
+        var context = CreateContext(newValues, UserValidationsRules.Edit);
+        
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, null, errors);
+        }
+        
+        
         var updatedUser = await _userHandler.EditUserDetails(newValues);
-        return (updatedUser != null, updatedUser);
+        return (updatedUser != null, updatedUser,new List<string>());
     }
 
-    public async Task<(bool Success, User? UpdatedUser)> TryChangeUserPassword(string newPassword, User user)
+    public async Task<(bool Success, User? UpdatedUser,List<string> errors)> TryChangeUserPassword(string newPassword, User user)
     {
+        var context = CreateContext(user, UserValidationsRules.ChangePassword);
+        
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        var newPasswordValidationResult = _userInputValidator.ValidateSingleString_ForPassword(newPassword);
+        
+        if (!validationResult.IsValid || newPasswordValidationResult.Success)
+        {
+            var model_errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            
+            model_errors.AddRange(newPasswordValidationResult.errors);
+
+            return (false, null, model_errors);
+        }
+        
         var result = await _userHandler.ChangeUserPassword(newPassword, user);
-        return (result != null, result);
+        return (result != null, result, new List<string>());
     }
-    public async Task<(bool Success, int UserId)> TryLoginUser(User user)
+    public async Task<(bool Success, int UserId,List<string> errors)> TryLoginUser(User user)
     {
+
+        var context = CreateContext(user, UserValidationsRules.Login);
+        
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+        
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, -1, errors);
+        }
+        
         var result = await _userHandler.LoginUser(user);
-        return (result != -1, result);  
+        return (result != -1, result,new List<string>());  
     }
 
-    public async Task<(bool Success, List<Income> Incomes)> TryGetUserIncomes(User user)
+    public async Task<(bool Success, List<Income> Incomes,List<string> errors)> TryGetUserIncomes(User user)
     {
+        var context = CreateContext(user, UserValidationsRules.GetUserData);
+
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, new List<Income>(), errors);
+        }
+        
         var incomes = await _userHandler.GetUserIncomes(user);
-        return (incomes.Count > 0, incomes);
+        return (incomes.Count > 0, incomes, new List<string>());
     }
 
-    public async Task<(bool Success, List<Budget> Budgets)> TryGetUserBudgets(User user)
+    public async Task<(bool Success, List<Budget> Budgets,List<string> errors)> TryGetUserBudgets(User user)
     {
+        var context = CreateContext(user, UserValidationsRules.GetUserData);
+
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, new List<Budget>(), errors);
+        }
+        
         var budgets = await _userHandler.GetUserBudgets(user);
-        return (budgets.Count > 0, budgets);
+        return (budgets.Count > 0, budgets,new List<string>());
     }
 
-    public async Task<(bool Success, List<Expense> Expenses)> TryGetUserExpenses(User user)
+    public async Task<(bool Success, List<Expense> Expenses,List<string> errors)> TryGetUserExpenses(User user)
     {
+        var context = CreateContext(user, UserValidationsRules.GetUserData);
+
+        var validationResult = await _userInputValidator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+            return (false, new List<Expense>(), errors);
+        }
+        
         var expenses = await _userHandler.GetUserExpenses(user);
-        return (expenses.Count > 0, expenses);
+        return (expenses.Count > 0, expenses,new List<string>());
     }
+
+
+
+    private ValidationContext<User> CreateContext(User user, UserValidationsRules validationsRules)
+    {
+        var context = new ValidationContext<User>(user,new PropertyChain(),
+            new RulesetValidatorSelector(new[] { validationsRules.ToString() }));
+
+        return context;
+    } 
 }
---
+++ Controller/Validators/UserInputValidator.cs
@@ -0,0 +1,96 @@
+using FluentValidation;
+using Model.Entities;
+
+namespace Controller.Validators;
+
+public class UserInputValidator : AbstractValidator<User>
+{
+    public UserInputValidator()
+    {
+        RuleSet(UserController.UserValidationsRules.Register.ToString(), () =>
+        {
+            RuleFor(u => u.Id).Empty();
+
+            RuleFor(u => u.UserName).NotEmpty().WithMessage("UserName can't Be Empty")
+                .MinimumLength(4).WithMessage("UserName have to be at least 4")
+                .MaximumLength(100).WithMessage("UserName Maximum can be 100 character");
+
+            RuleFor(u => u.Email).NotEmpty().WithMessage("Email Can't Be Empty")
+                .EmailAddress().WithMessage("This is not a valid Email")
+                .MaximumLength(100).WithMessage("Maximum Length is 100");
+
+            RuleFor(u => u.PhoneNumber).NotEmpty().WithMessage("Phone Number can't be Empty")
+                .Matches("^01[0125]\\d{8}$\n").WithMessage("Not a valid Egypt PhoneNumber, try again");
+
+            RuleFor(u => u.Password).NotEmpty().WithMessage("Password Can't Be Empty")
+                .MinimumLength(6).WithMessage("Minimum Password Length is 6")
+                .MaximumLength(255).WithMessage("Maximum Password Length is 255");
+        });
+        
+        
+        
+        
+        RuleSet(UserController.UserValidationsRules.Login.ToString(), () =>
+        {
+            RuleFor(u => u.Id).Empty().WithMessage("Don't Include Id in login,(developer Error)");
+            
+            RuleFor(u => u.Email).NotEmpty().WithMessage("Email Can't Be Empty")
+                .EmailAddress().WithMessage("This is not a valid Email")
+                .MaximumLength(100).WithMessage("Maximum Length is 100");
+        
+            RuleFor(u => u.Password).NotEmpty().WithMessage("Password Can't Be Empty")
+                .MinimumLength(6).WithMessage("Minimum Password Length is 6")
+                .MaximumLength(255).WithMessage("Maximum Password Length is 255");
+        });
+        
+        
+        RuleSet(UserController.UserValidationsRules.Edit.ToString(), () =>
+        {
+            RuleFor(u => u.Id).NotEmpty().WithMessage("Id Can't Be Empty if your trying to Edit");
+            
+            RuleFor(u => u.UserName).NotEmpty().WithMessage("UserName can't Be Empty")
+                .MinimumLength(4).WithMessage("UserName have to be at least 4")
+                .MaximumLength(100).WithMessage("UserName Maximum can be 100 character");
+
+            RuleFor(u => u.Email).NotEmpty().WithMessage("Email Can't Be Empty")
+                .EmailAddress().WithMessage("This is not a valid Email")
+                .MaximumLength(100).WithMessage("Maximum Length is 100");
+
+            RuleFor(u => u.PhoneNumber).NotEmpty().WithMessage("Phone Number can't be Empty")
+                .Matches("^01[0125]\\d{8}$\n").WithMessage("Not a valid Egypt PhoneNumber, try again");
+
+            RuleFor(u => u.Password).Empty()
+                .WithMessage("Don't Include Password in the input if your editing details only");
+        });
+
+
+        RuleSet(UserController.UserValidationsRules.ChangePassword.ToString(),
+            () =>
+            {
+                RuleFor(u => u.Id).NotEmpty().WithMessage("Id Can't Be Empty When Changing Password");
+
+                RuleFor(u => u.PhoneNumber).NotEmpty().WithMessage("Old Password Must be Entered").MinimumLength(6)
+                    .WithMessage("Minimum Length is 6 characters for password");
+            });
+
+        RuleSet(
+            UserController.UserValidationsRules.GetUserData.ToString(),
+            () =>
+            {
+                RuleFor(u => u.Id).NotEmpty().WithMessage("Can't Leave Id Empty if Accessing Data");
+            }
+            );
+    }
+    
+    public (bool Success,List<string> errors) ValidateSingleString_ForPassword(string input)
+    {
+        var errors = new List<string>();
+        var stringValidator = new InlineValidator<string>();
+        stringValidator.RuleFor(x => x).NotEmpty().WithMessage("New Password Can't Be Empty");
+        stringValidator.RuleFor(x => x).MinimumLength(4).WithMessage("Password Length must be more than 6 characters");
+
+        var validationResult = stringValidator.Validate(input);
+        errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+        return (validationResult.IsValid,errors);
+    }
+}
